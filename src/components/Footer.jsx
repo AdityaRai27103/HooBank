54abb8d26b6be030585071ce9345c8d9da07c00a
AdityaRai27103 <adityarai27103@gmail.com>
2024-07-11 14:21:28 +0530

    footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]">
       <p className="font-poppins font-normal text-[18px] leading-[27px] text-white text-center">
-        2023 HooBank. All Right Reserved.
+        Made by Aditya Rai ❤️
       </p>
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
